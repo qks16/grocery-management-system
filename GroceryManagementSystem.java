e2f36baff5c9913983d1a4238ecf22c5db1b2b31
qks16 <qks16@txstate.edu>
2026-09-16 09:45:56 -0500

    initial commit

---
+++ GroceryManagementSystem.java
@@ -0,0 +1,9 @@
+public class GroceryManagementSystem {
+
+    public static void main(String[] args) {
+        // Main method implementation goes here
+        String[] itemNames = new String[10];
+        double[] itemPrices = new double[10];
+        int[] itemStocks = new int[10];
+    }
+}
